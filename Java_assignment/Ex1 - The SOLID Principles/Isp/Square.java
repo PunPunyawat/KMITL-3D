da99f26ec9faa83e93ab5d893676a14789b57e80
thepunnn <69188795+PunPunyawat@users.noreply.github.com>
2022-10-12 22:25:19 +0700

    The SOLID

---
+++ Java_assignment/Ex1 - The SOLID Principles/Isp/Square.java
@@ -0,0 +1,23 @@
+public class Square extends Shape {
+
+    private int side;
+
+    Square(int side) {
+        this.setSide(side);
+    }
+
+    public void setSide(int side){
+        this.side = side;
+    }
+
+    @Override
+    public int getWidth(){
+        return side;
+    }
+
+    @Override
+    public int getArea(){
+        return side * side;
+    }
+}
+
